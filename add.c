5cd36362f06e79728d8588f73e145399a3024bf3
Saurabh Srivastava <ssrivastava@iitism.ac.in>
2024-10-15 12:50:35 +0530

    Added user inputs; also added .gitignore

--- add.c
+++ add.c
@@ -2,8 +2,13 @@
 
 int main()
 {
-    int a = 5, b = 8;
-    int sum = a + b;
+    int a, b;
+    int sum;
+    printf("Enter first integer: ");
+    scanf("%d", &a);
+    printf("Enter second integer: ");
+    scanf("%d", &b);
+    sum = a + b;
     printf("The sum of %d and %d is %d\n", a, b, sum);
     return 0;
 }
