7d36f94f773e001b3c31c33886466f936dfb8c7d
Saurabh Srivastava <ssrivastava@iitism.ac.in>
2024-10-15 15:17:33 +0530

    Implemented the experimental, add() function

--- add.c
+++ add.c
@@ -1,14 +1,27 @@
 #include<stdio.h>
 
+int add(int i, int j)
+{
+    return (i + j);
+}
+
 int main()
 {
     int a, b;
     int sum;
-    printf("Enter first integer: ");
-    scanf("%d", &a);
-    printf("Enter second integer: ");
-    scanf("%d", &b);
-    sum = a + b;
-    printf("The sum of %d and %d is %d\n", a, b, sum);
+    char confirm = 'n';
+    do
+    {
+        printf("Enter first integer: ");
+        scanf("%d", &a);
+        printf("Enter second integer: ");
+        scanf("%d", &b);
+        sum = add(a, b);
+        printf("The sum of %d and %d is %d\n", a, b, sum);
+        printf("Do you have more sums for me [y/N]? ");
+        scanf(" %c", &confirm);
+    }
+    while(confirm == 'y' || confirm == 'Y');
     return 0;
 }
+
