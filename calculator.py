8e19d43cc578c4f13bf06f41208635e8da1114bb
Saurabh Srivastava <ssrivastava@iitism.ac.in>
2025-02-06 14:26:40 +0530

    Initial commit: Add calculator with add and subtract functions

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Simple Calculator")
+    x = 10
+    y = 5
+    print("Add:", add(x, y))
+    print("Subtract:", subtract(x, y))
